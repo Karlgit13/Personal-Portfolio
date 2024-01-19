910f5f1ce51b8900220c7c4312fcdf2d53cd4f34
Karlgit13 <karl.vareskog@gmail.com>
2024-01-19 17:12:11 +0100

    Add certificate images to Section3 component

--- src/components/Section3.jsx
+++ src/components/Section3.jsx
@@ -1,10 +1,67 @@
 import React from "react";
 import "../style/section3.css";
 
+const certificates = [
+  {
+    id: 1,
+    image: require("../assets/AdvancedReact.jpg"),
+    alt: "AdvancedReact",
+  },
+  {
+    id: 2,
+    image: require("../assets/DeveloperCapstone.jpg"),
+    alt: "DeveloperCapstone",
+  },
+  {
+    id: 3,
+    image: require("../assets/HTML and CSS in depth_page-0001.jpg"),
+    alt: "HTMLCSS",
+  },
+  {
+    id: 4,
+    image: require("../assets/IntroFrontEnd_Meta_page-0001.jpg"),
+    alt: "IntroFrontEnd",
+  },
+  {
+    id: 5,
+    image: require("../assets/Principles of UX-UI Design_page-0001.jpg"),
+    alt: "UX-UI",
+  },
+  {
+    id: 6,
+    image: require("../assets/Programming_With_Javascript_Meta_page-0001.jpg"),
+    alt: "ProgrammingWithJavascript",
+  },
+  {
+    id: 7,
+    image: require("../assets/Python_Mimo_page-0001.jpg"),
+    alt: "Python",
+  },
+  {
+    id: 8,
+    image: require("../assets/React_Basics_Meta_page-0001.jpg"),
+    alt: "ReactBasics",
+  },
+  {
+    id: 9,
+    image: require("../assets/Version_Controll_Meta_page-0001.jpg"),
+    alt: "VersionControl",
+  },
+];
+
 const Section3 = () => {
   return (
     <div id="section3" className="section3">
-      Section3
+      <div className="certs-container">
+        {certificates.map((certificate) => (
+          <img
+            key={certificate.id}
+            className="certImage"
+            src={certificate.image}
+            alt={certificate.alt}
+          />
+        ))}
+      </div>
     </div>
   );
 };
