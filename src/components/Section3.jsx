09eed6aabbbcaaa8534140aee9ddb14e4eccc936
Karlgit13 <karl.vareskog@gmail.com>
2024-01-19 21:59:14 +0100

    Add Accordion component to Section3.jsx

--- src/components/Section3.jsx
+++ src/components/Section3.jsx
@@ -1,5 +1,11 @@
-import React from "react";
 import "../style/section3.css";
+import * as React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 const certificates = [
   {
@@ -59,25 +65,38 @@ const Section3 = () => {
         ))}
       </div>
       <div className="infoDiv">
-        <h1 className="intoTitle">Om mig</h1>
-        <p className="infoText">
-          Med 28 år i bagaget och en uppväxt i Tyresö, har jag sedan tidig
-          ungdom drivits av ett djupt intresse för programmering och
-          mjukvaruutveckling. Min väg har dock varit allt annat än rak. Efter
-          att ha fullföljt en gymnasieutbildning inom el-teknik och utforskat
-          olika yrkesvägar, insåg jag att det var i kodens värld jag verkligen
-          hörde hemma. Det är först nyligen som jag bestämt mig för att
-          helhjärtat satsa på min passion. Jag har påbörjat en
-          spelutvecklingsutbildning via Komvux samt ett
-          frontend-utvecklingsprogram genom META. Dessa studier har inte bara
-          bekräftat min kärlek för fältet utan också skärpt min förmåga att ta
-          mig an komplicerade projekt. Jag är en person som trivs med att
-          ständigt utmanas och ser varje nytt projekt som en möjlighet att växa.
-          Programmering är inte bara ett yrke för mig – det är en livslång
-          passion och det jag vill ägna min karriär åt. Jag ser fram emot att
-          dyka ner i komplexa problem och fortsätta växa min kompetens
-          tillsammans med likasinnade i branschen.
-        </p>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ArrowDownwardIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+            <Typography>Accordion 1</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ArrowDropDownIcon />}
+            aria-controls="panel2-content"
+            id="panel2-header"
+          >
+            <Typography>Accordion 2</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
       </div>
     </div>
   );
