592c602d8a34fd8b4f27a391e45036a948fa19d1
Karlgit13 <karl.vareskog@gmail.com>
2024-01-25 15:22:42 +0100

    Remove unused code and dependencies, update Section2 component

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -1,9 +1,4 @@
-import * as React from "react";
-import Box from "@mui/joy/Box";
-import Card from "@mui/joy/Card";
-import CardCover from "@mui/joy/CardCover";
-import CardContent from "@mui/joy/CardContent";
-import Typography from "@mui/joy/Typography";
+import React from "react";
 import "../style/section2.css";
 import LittleLemon from "../assets/LittleLemon.png";
 
@@ -13,96 +8,6 @@ const Section2 = () => {
       <div className="heading-div">
         <h1 className="section2-heading">Mina Projekt</h1>
       </div>
-      <Box
-        className="box"
-        sx={{
-          padding: "30px",
-          perspective: "1000px",
-          transition: "transform 0.4s",
-          "& > div, & > div > div": {
-            transition: "inherit",
-          },
-          "&:hover": {
-            "& > div": {
-              transform: "rotateY(30deg)",
-              "& > div:nth-child(2)": {
-                transform: "scaleY(0.9) translate3d(20px, 30px, 40px)",
-              },
-              "& > div:nth-child(3)": {
-                transform: "translate3d(45px, 50px, 40px)",
-              },
-            },
-          },
-        }}
-      >
-        <Card
-          variant="outlined"
-          sx={{
-            maxHeight: "200px",
-            minHeight: "130px",
-            width: 320,
-            backgroundColor: "#fff",
-            borderColor: "#000",
-          }}
-        >
-          <CardCover></CardCover>
-          <CardContent>
-            <img className="projekt-image" src={LittleLemon} alt="" />
-          </CardContent>
-        </Card>
-      </Box>
-      <Box
-        className="box"
-        sx={{
-          perspective: "1000px",
-          transition: "transform 0.4s",
-          "& > div, & > div > div": {
-            transition: "inherit",
-          },
-          "&:hover": {
-            "& > div": {
-              transform: "rotateY(30deg)",
-              "& > div:nth-child(2)": {
-                transform: "scaleY(0.9) translate3d(20px, 30px, 40px)",
-              },
-              "& > div:nth-child(3)": {
-                transform: "translate3d(45px, 50px, 40px)",
-              },
-            },
-          },
-        }}
-      >
-        <Card
-          variant="outlined"
-          sx={{
-            minHeight: "100px",
-            width: 320,
-            backgroundColor: "#fff",
-            borderColor: "#000",
-          }}
-        >
-          <Typography level="h2" fontSize="lg" textColor="#000">
-            Little Lemon Restaurant
-          </Typography>
-          <CardCover
-            sx={{
-              background:
-                "linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 200px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 300px)",
-              border: "1px solid",
-              borderColor: "#777",
-            }}
-          >
-            <Typography level="h2" fontSize="lg" textColor="#fff">
-              Card Cover
-            </Typography>
-          </CardCover>
-          <CardContent>
-            <Typography level="h2" fontSize="lg" textColor="#fff" m={2}>
-              Card Content
-            </Typography>
-          </CardContent>
-        </Card>
-      </Box>
     </div>
   );
 };
