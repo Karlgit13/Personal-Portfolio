b33b00c20ec83fb106ca9dc97b6ab16a66d47335
Karlgit13 <karl.vareskog@gmail.com>
2024-01-25 19:21:42 +0100

    Add react-slick and slick-carousel dependencies

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "../style/section2.css";
 
 const handleClick = (url) => {
@@ -6,21 +9,33 @@ const handleClick = (url) => {
 };
 
 const Section2 = ({ ProjektLista }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    className: "Karusell",
+  };
+
   return (
     <div id="section2" className="section2">
       <div className="heading-div">
         <h1 className="section2-heading">Mina Projekt</h1>
       </div>
-      <div>
+      <Slider {...settings}>
         {ProjektLista.map(({ titel, img, url }) => (
           <div key={titel} className="projekt-div">
             <h2 className="projekt-titel">{titel}</h2>
             <img className="projekt-bild" src={img} alt={titel} />
-            <p className="projekt-paragraph">info om projektet</p>
-            <button onClick={() => handleClick(url)}>LiveDemo</button>
+            <p className="projekt-paragraph">Beskrivning av projektet</p>
+            <button className="projekt-knapp" onClick={() => handleClick(url)}>
+              LiveDemo
+            </button>
           </div>
         ))}
-      </div>
+      </Slider>
     </div>
   );
 };
