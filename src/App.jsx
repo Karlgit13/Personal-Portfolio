e197461f27111e7d55cf47bf4e86d0f6aaaf5210
Karlgit13 <karl.vareskog@gmail.com>
2024-01-28 13:25:16 +0100

    Update live demo button text and adjust certificate container width

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const ProjektLista = [
     titel: "Little Lemon Restaurant",
     img: require("./assets/LittleLemon.png"),
     url: "https://karlgit13.github.io/React-Little-Lemon-Restaurant/",
-    buttonText: "LIVE => DEMO",
+    buttonText: "LIVE DEMO =>",
     beskrivning:
       "Det här är mitt första projekt i React.js, som jag utvecklade under tiden jag genomgick ett online-kurspaket i Front-end-utveckling. Det har varit en enormt givande erfarenhet som inte bara utvecklat mina tekniska färdigheter, utan också gett mig insikter i responsiv design för både desktop och mobila enheter.",
   },
