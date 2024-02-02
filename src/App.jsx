d9fbcc9f310b6d6e32173e5068068f1f7de8f0b3
Karlgit13 <karl.vareskog@gmail.com>
2024-02-02 19:55:24 +0100

    Add Tränings-Tracker project to ProjektLista

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,13 @@ const ProjektLista = [
       "Det här är mitt första projekt i React.js, som jag utvecklade under tiden jag genomgick ett online-kurspaket i Front-end-utveckling. Det har varit en enormt givande erfarenhet som inte bara utvecklat mina tekniska färdigheter, utan också gett mig insikter i responsiv design för både desktop och mobila enheter.",
   },
   {
+    titel: 'Tränings-Tracker',
+    img: require("./assets/gymTracker.png"),
+    url: "https://karlgit13.github.io/Tr-nings-Tracker/",
+    buttonText: "LIVE DEMO =>",
+    beskrivning: "Mitt problem: Jag gillar att gymma i stortsätt varje dag men är inte ett fan av att följa strikta gymscheman. Min lösning: En app som hjälper mig att hålla koll på vilka muskelgrupper jag har tränat under veckans gång (så jag får med hela kroppen) och även återhämtningstiden för musklerna. PS. detta är ett enkelt projekt skapat för att hjälpa mig. Projektet använder ingen databas utan använder sig av lokalminnet i webbläsaren och är inställd på att återställas när en ny vecka börjar!",
+  },
+  {
     titel: '"UNDER CONSTRUCTION"',
     img: require("./assets/UnderConstruction.png"),
     url: "",
