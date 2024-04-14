03d10c5c1e46e458764eb8f4da4bb063797ab05c
Karlgit13 <karl.vareskog@gmail.com>
2024-04-14 07:49:49 +0200

    Remove "UNDER CONSTRUCTION" project from ProjektLista

--- src/App.jsx
+++ src/App.jsx
@@ -48,14 +48,6 @@ const ProjektLista = [
     beskrivning:
       "Det här är mitt första projekt i React.js, som jag utvecklade under tiden jag genomgick ett online-kurspaket i Front-end-utveckling. Det har varit en enormt givande erfarenhet som inte bara utvecklat mina tekniska färdigheter, utan också gett mig insikter i responsiv design för både desktop och mobila enheter.",
   },
-
-  {
-    titel: '"UNDER CONSTRUCTION"',
-    img: require("./assets/UnderConstruction.png"),
-    url: "",
-    buttonText: null,
-    beskrivning: "",
-  },
 ];
 
 export default function App() {
